25a92b88e20185eada38425fb9af1f6ec634ffc8
UniversoPrematuro <universoprematuro@gmail.com>
2022-08-25 10:11:32 -0300

    initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+void main() async {
+  WidgetsFlutterBinding().ensureVisualUpdate();
+  await Firebase.initializeApp();
+  runApp(ModularApp(module: AppModule(), child:  const AppWidget()));}
